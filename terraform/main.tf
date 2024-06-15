50c0f7d796294782646e79f5f1648cf8793f8f81
L00179208 <L00179208@atu.ie>
2024-06-15 01:31:09 +0100

    SSH Adding process moved to teraform, fixed duplicate

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,10 @@ provider "aws" {
   region = "eu-west-2" # Update to your region
 }
 
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "academic-key-pair"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-}
-
 resource "aws_instance" "docker" {
   ami           = "ami-053a617c6207ecc7b" # Update to your AMI
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = "academic-key-pair" # Use your existing key pair name
 
   tags = {
     Name = "DockerInstance"
@@ -24,9 +14,4 @@ resource "aws_instance" "docker" {
 
 output "instance_ip" {
   value = aws_instance.docker.public_ip
-}
-
-output "private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
 }
