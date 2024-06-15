2434724f0145a8a89588de6d220ea29fbf1e5f3f
L00179208 <L00179208@atu.ie>
2024-06-15 01:24:25 +0100

    SSH Adding process moved to teraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,36 @@
 provider "aws" {
-  region = "eu-west-2" # Change this to your desired region
+  region = "eu-west-2" # Update to your region
 }
 
-variable "key_name" {
-  description = "Name of the SSH key pair"
-  type        = string
-  default     = "academic-key-pair" # Change this to your key pair name
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "academic-key-pair"
+  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 resource "aws_instance" "docker" {
-  ami           = "ami-053a617c6207ecc7b" # Example AMI ID, replace with your own
+  ami           = "ami-053a617c6207ecc7b" # Update to your AMI
   instance_type = "t2.micro"
-  key_name      = var.key_name
+  key_name      = aws_key_pair.deployer.key_name
 
-  # Additional configurations...
+  tags = {
+    Name = "DockerInstance"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > private_key.pem"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.docker.public_ip
+}
+
+output "private_key" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
