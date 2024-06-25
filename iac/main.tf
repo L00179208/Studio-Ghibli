9e988b076b31de93534f8f962507c5e111d72fc6
L00179208 <L00179208@atu.ie>
2024-06-25 14:30:53 +0100

    Modified Terrfarom script to excelude access key

--- iac/main.tf
+++ iac/main.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  region     = "eu-west-2"
-  alias      = "spinup_aws"
-  access_key = "AKIAYNTW3YLSKHEBGDOG"
-  secret_key = "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"
+  region = "eu-west-2"
+  alias  = "spinup_aws"
 }
