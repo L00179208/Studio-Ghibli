4e2cd15067a083b4356ae4ac496799b141f14274
L00179208 <L00179208@atu.ie>
2024-06-26 13:47:16 +0100

    Connect and deploy to ec2_28| Public Subnet Assocation Fixed

--- iac/modules/network/main.tf
+++ iac/modules/network/main.tf
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "stg_app_gateway" {
     Name = "STG App Gateway"
   }
 }
-
+# Public Route Table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
 
