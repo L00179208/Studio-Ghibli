157d184eb4d036bfdfb5bf205e5453a4aaff6d58
L00179208 <L00179208@atu.ie>
2024-06-28 15:37:35 +0100

    IP Address missing issue fixed in Deplo Stage | 3

--- iac/modules/ec2_instance/main.tf
+++ iac/modules/ec2_instance/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "instance" {
   key_name      = var.key_name
   subnet_id     = var.subnet_id
 
+  provisioner "file" {
+    source      = "../../id_rsa.pub"
+    destination = "/home/ubuntu/.ssh/authorized_keys"
+  }
+
   tags = {
     Name = var.instance_name
   }
